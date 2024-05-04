c0045e0f6994f47e2ca65d2092668305900af9a6
Toni Adiwijaya <toniadiwijaya7@gmail.com>
2024-05-04 22:53:03 +0700

    menambahkan pargaraf

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Hello World!</h1>
+    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Doloremque cumque ratione itaque quidem qui aut suscipit sunt repudiandae. Ut numquam nobis placeat quibusdam nostrum nam et optio officia eligendi fugiat.</p>
 </body>
 </html>
